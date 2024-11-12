40a17b20308f0b8d1954ccffda77beebab5cc7f7
usermemmdli <mustafamemmedli005@mail.ru>
2024-11-13 01:09:42 +0400

    update project

--- src/main/java/com/example/bc_spring_app/BankCustomerAppAdminApplication.java
+++ src/main/java/com/example/bc_spring_app/BankCustomerAppAdminApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankCustomerAppAdmin {
+public class BankCustomerAppAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankCustomerAppAdmin.class, args);
+		SpringApplication.run(BankCustomerAppAdminApplication.class, args);
 	}
 }
